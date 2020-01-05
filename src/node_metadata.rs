8c53dacd03461aac07b18e84431a23a307e0cd9a
Timothy Whitehead <twhiteheadzm@gmail.com>
2020-01-05 17:02:06 +0200

    metadata update

--- src/node_metadata.rs
+++ src/node_metadata.rs
@@ -140,7 +140,7 @@ impl Arg {
 pub fn parse_metadata(metadata: &RuntimeMetadataPrefixed) -> Vec<Module> {
     let mut mod_vec = Vec::<Module>::new();
     match &metadata.1 {
-        RuntimeMetadata::V9(value) => {
+        RuntimeMetadata::V10(value) => {
             match &value.modules {
                 DecodeDifferent::Decoded(mods) => {
                     let modules = mods;
